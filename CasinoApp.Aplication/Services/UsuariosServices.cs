1fb2b76812b2071f3d3a2ed41de078476323a913
yer546987 <beltranmosquera1020@gmail.com>
2023-11-13 22:07:54 -0500

    Add userController

--- CasinoApp.Aplication/Services/UsuariosServices.cs
+++ CasinoApp.Aplication/Services/UsuariosServices.cs
@@ -1,6 +1,9 @@
 ﻿using CasinoApp.Aplication.Contracts;
+using CasinoApp.Aplication.DataAccess;
+using CasinoApp.Aplication.Models;
 using CasinoApp.Entities.Http;
 using CasinoApp.Entities.Usuario;
+using Microsoft.EntityFrameworkCore;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -11,9 +14,38 @@ namespace CasinoApp.Aplication.Services
 {
     public class UsuariosServices : IUsuarioServices
     {
+        private CasinoAppContext _Context;
+
+        public UsuariosServices() 
+        {
+            _Context = new CasinoAppContext();
+        }
         public RequestResult<UsuarioDto> Create(UsuarioDto usuario)
         {
-            throw new NotImplementedException();
+            try
+            {
+                if (usuario is null)
+                    return RequestResult<UsuarioDto>.CreateNoSuccess("Los datos son requeridos");
+                if (string.IsNullOrEmpty(usuario.Nombre))
+                    return RequestResult<UsuarioDto>.CreateNoSuccess("El nombre del usuario es requerido");
+                Usuario entity = new Usuario();
+                entity.Nombre = usuario.Nombre;
+                var result = _Context.Usuarios.Add(entity);
+                int rows = _Context.SaveChanges();
+                if (rows is 0)
+                    return RequestResult<UsuarioDto>.CreateNoSuccess("Ha ocurrido un error al registrar el usuario.");
+                var resultado = new UsuarioDto()
+                {
+                    Id = result.Entity.Id,
+                    Nombre = result.Entity.Nombre,
+                    Usuario1 = result.Entity.Usuario1
+                };
+                return RequestResult<UsuarioDto>.CreateSuccess(resultado);
+            }
+            catch (Exception ex)
+            {
+                return RequestResult<UsuarioDto>.CreateError(ex.Message);
+            }
         }
 
         public bool Delete(int isUsuario)
@@ -23,17 +55,67 @@ namespace CasinoApp.Aplication.Services
 
         public RequestResult<List<UsuarioDto>> GetAll()
         {
-            throw new NotImplementedException();
+            try
+            {
+                var usuario = _Context.Usuarios.ToList();
+                List<UsuarioDto> result = new List<UsuarioDto>();
+                foreach (var item in usuario)
+                {
+                    result.Add(new UsuarioDto()
+                    {
+                        Id = item.Id,
+                        Nombre = item.Nombre,
+                        Usuario1 = item.Usuario1
+                    });
+                }
+                return RequestResult<List<UsuarioDto>>.CreateSuccess(result);
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
         }
 
         public RequestResult<UsuarioDto> GetById(int idUsuario)
         {
-            throw new NotImplementedException();
+            try
+            {
+                var usuario = _Context.Usuarios.Where(x => x.Id == idUsuario).FirstOrDefault();
+                if (usuario is null) return RequestResult<UsuarioDto>.CreateNoSuccess($"No existe el usuario con identificador {idUsuario}");
+                var resultado = new UsuarioDto()
+                {
+                    Id = usuario.Id,
+                    Nombre = usuario.Nombre,
+                    Usuario1 = usuario.Usuario1
+                };
+                return RequestResult<UsuarioDto>.CreateSuccess(resultado);
+            }
+            catch (Exception ex)
+            {
+                return RequestResult<UsuarioDto>.CreateError($"Ha ocurrido un error: {ex.Message}");
+            }
         }
 
         public UsuarioDto Update(UsuarioDto usuario)
         {
-            throw new NotImplementedException();
+            if (usuario is null) return null;
+            if (usuario.Id is 0) return null;
+            if (string.IsNullOrEmpty(usuario.Nombre)) return null;
+            var entidad = _Context.Usuarios
+                                .Where(x => x.Id.Equals(usuario.Id))
+                                .FirstOrDefault();
+            if (entidad == null) return null;
+            entidad.Nombre = usuario.Nombre;
+            _Context.Attach(entidad);
+            _Context.Entry(entidad).State = EntityState.Modified;
+            _Context.SaveChanges();
+            return new UsuarioDto()
+            {
+                Id = entidad.Id,
+                Nombre = entidad.Nombre,
+                Usuario1 = entidad.Usuario1
+            };
         }
     }
 }
