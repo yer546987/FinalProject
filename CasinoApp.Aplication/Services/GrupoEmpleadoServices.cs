7b8bd80e1620bad38ad5ab9311027221cb00f0a0
yer546987 <beltranmosquera1020@gmail.com>
2023-11-14 13:10:31 -0500

    Add GrupoEmpleadoServices Api

--- CasinoApp.Aplication/Services/GrupoEmpleadoServices.cs
+++ CasinoApp.Aplication/Services/GrupoEmpleadoServices.cs
@@ -1,6 +1,10 @@
 ﻿using CasinoApp.Aplication.Contracts;
+using CasinoApp.Aplication.DataAccess;
+using CasinoApp.Aplication.Models;
 using CasinoApp.Entities.GrupoEmpleado;
 using CasinoApp.Entities.Http;
+using CasinoApp.Entities.MovimientoCasino;
+using Microsoft.EntityFrameworkCore;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -11,9 +15,38 @@ namespace CasinoApp.Aplication.Services
 {
     public class GrupoEmpleadoServices : IGrupoEmpleadoServices
     {
+        private CasinoAppContext _Context;
+
+        public GrupoEmpleadoServices() 
+        {
+            _Context = new CasinoAppContext();
+        }
         public RequestResult<GrupoEmpleadoDto> Create(GrupoEmpleadoDto grupoEmpleado)
         {
-            throw new NotImplementedException();
+            try
+            {
+                if (grupoEmpleado is null)
+                    return RequestResult<GrupoEmpleadoDto>.CreateNoSuccess("Los datos son requeridos");
+                if (string.IsNullOrEmpty(grupoEmpleado.NombreGrupo))
+                    return RequestResult<GrupoEmpleadoDto>.CreateNoSuccess("El nombre del Grupo del empleado es requerido");
+                GrupoEmpleado entity = new GrupoEmpleado();
+                entity.NombreGrupo = grupoEmpleado.NombreGrupo;
+                var result = _Context.GrupoEmpleados.Add(entity);
+                int rows = _Context.SaveChanges();
+                if (rows is 0)
+                    return RequestResult<GrupoEmpleadoDto>.CreateNoSuccess("Ha ocurrido un error al registrar el grupo del empleado.");
+                var resultado = new GrupoEmpleadoDto()
+                {
+                    Id = result.Entity.Id,
+                    NombreGrupo = result.Entity.NombreGrupo
+                };
+                return RequestResult<GrupoEmpleadoDto>.CreateSuccess(resultado);
+
+            }
+            catch (Exception ex)
+            {
+                return RequestResult<GrupoEmpleadoDto>.CreateError(ex.Message);
+            }
         }
 
         public bool Delete(int idGrupoEmpleado)
@@ -23,17 +56,66 @@ namespace CasinoApp.Aplication.Services
 
         public RequestResult<List<GrupoEmpleadoDto>> GetAll()
         {
-            throw new NotImplementedException();
+            try
+            {
+                var grupoEmpleado = _Context.GrupoEmpleados.ToList();
+                List<GrupoEmpleadoDto> result = new List<GrupoEmpleadoDto>();
+                foreach (var item in grupoEmpleado)
+                {
+                    result.Add(new GrupoEmpleadoDto()
+                    {
+                        Id = item.Id,
+                        NombreGrupo = item.NombreGrupo
+                    });
+                }
+                return RequestResult<List<GrupoEmpleadoDto>>.CreateSuccess(result);
+
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
         }
 
         public RequestResult<GrupoEmpleadoDto> GetById(int idGrupoEmpleado)
         {
-            throw new NotImplementedException();
+            try
+            {
+                var grupoEmpleado = _Context.GrupoEmpleados.Where(x => x.Id == idGrupoEmpleado).FirstOrDefault();
+                if (grupoEmpleado is null) return RequestResult<GrupoEmpleadoDto>.CreateNoSuccess($"No existe el grupo de empleado con identificador {idGrupoEmpleado}");
+                var resultado = new GrupoEmpleadoDto()
+                {
+                    Id = grupoEmpleado.Id,
+                    NombreGrupo = grupoEmpleado.NombreGrupo
+                };
+                return RequestResult<GrupoEmpleadoDto>.CreateSuccess(resultado);
+
+            }
+            catch (Exception ex)
+            {
+                return RequestResult<GrupoEmpleadoDto>.CreateError($"Ha ocurrido un error : {ex.Message}");
+            }
         }
 
         public GrupoEmpleadoDto Update(GrupoEmpleadoDto grupoEmpleado)
         {
-            throw new NotImplementedException();
+            if (grupoEmpleado is null) return null;
+            if (grupoEmpleado.Id is 0) return null;
+            if (string.IsNullOrEmpty(grupoEmpleado.NombreGrupo)) return null;
+            var entidad = _Context.GrupoEmpleados
+                                .Where(x => x.Id.Equals(grupoEmpleado.Id))
+                                .FirstOrDefault();
+            if (entidad == null) return null;
+            entidad.NombreGrupo = grupoEmpleado.NombreGrupo;
+            _Context.Attach(entidad);
+            _Context.Entry(entidad).State = EntityState.Modified;
+            _Context.SaveChanges();
+            return new GrupoEmpleadoDto()
+            {
+                Id = entidad.Id,
+                NombreGrupo = entidad.NombreGrupo
+            };
         }
     }
 }
