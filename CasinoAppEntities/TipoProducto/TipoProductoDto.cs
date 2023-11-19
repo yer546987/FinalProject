3ad63aa092ea779fcded4c92cf288550b7d926c3
yer546987 <beltranmosquera1020@gmail.com>
2023-11-19 11:06:52 -0500

    add product and TypeProduct Dto

---
+++ CasinoAppEntities/TipoProducto/TipoProductoDto.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Reflection.PortableExecutable;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CasinoApp.Entities.Producto
+{
+    public class TipoProductoDto
+    {
+        [Key]
+        public int IdTipoProducto { get; set; }
+        public string TipoProducto { get; set; }
+    }
+}
